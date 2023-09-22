d113c2aac0e49cd5dd23749689dc290eb51133eb
Sergey <aliger99@rambler.ru>
2023-09-22 22:05:20 +0700

    Lecture_2

--- Lecture/Lecture2/selenium/main.py
+++
@@ -1,9 +0,0 @@
-import yaml
-from module import Site
-
-with open("./testdata.yaml") as f:
-    testdata = yaml.safe_load(f)
-site = Site(testdata["address"])
-
-if __name__ == "__main__":
-    pass
